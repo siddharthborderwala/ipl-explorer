7f6c376015e24bfc6d2a3f953e3643375c4a6089
Siddharth Borderwala <siddharthborderwala@gmail.com>
2020-12-24 12:00:05 +0530

    create tags

---
+++ components/Tags.js
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+
+export default function Tags() {
+  return (
+    <Head>
+      <title>IPL Explorer | Siddharth Borderwala</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  )
+}
