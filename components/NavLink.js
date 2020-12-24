4f8677fa369e8ab6f3e2470454c26741c9686b36
Siddharth Borderwala <siddharthborderwala@gmail.com>
2020-12-24 16:40:38 +0530

    update styles

--- components/NavLink.js
+++ components/NavLink.js
@@ -2,11 +2,11 @@ export default function NavLink({ tabName, isActive }) {
   return (
     <a
       href={`#${tabName}`}
-      className={`border rounded-md focus:outline-none focus:ring-2 ${
+      className={`text-md inline-block border rounded-md focus:outline-none focus:ring-2 ${
         isActive
           ? 'bg-indigo-100 text-indigo-600 hover:bg-indigo-100'
           : ' text-gray-500'
-      }  px-5 py-2 capitalize hover:bg-indigo-50`}
+      } px-5 py-2.5 capitalize hover:bg-indigo-50`}
     >
       {tabName}
     </a>
